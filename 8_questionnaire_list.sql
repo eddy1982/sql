4263f933ea9e306c6aef0850d2f42e8f9c0d2799
eddy <yenhsun1982@gmail.com>
2016-10-24 10:49:31 +0800

    滿意度問券

--- 8_questionnaire_list.sql
+++ 8_questionnaire_list.sql
@@ -303,7 +303,7 @@ select a.userid, count(a.userid) as user_count
 from (
     SELECT userid, signin_time
     FROM plsport_playsport.member_signin_log_archive
-    where date(signin_time) between '2016-08-01' and '2016-08-31') as a /*要指定上個月, 例如3月時, 要寫2/1~2/28*/
+    where date(signin_time) between '2016-09-01' and '2016-09-30') as a /*要指定上個月, 例如3月時, 要寫2/1~2/28*/
 group by a.userid;
 
 
